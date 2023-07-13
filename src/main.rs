245055f80b3a4ce77b34eb02e85d75a130009dc6
andrei0230 <andrei.ciucle02@gmail.com>
2023-07-13 16:50:58 +0300

    working version

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::env;
+use std::fs;
+use std::process;
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let file_path: &&str = &args[1].trim();
+
+    match fs::read_to_string(file_path) {
+        Ok(content) => println!("{content}"),
+        Err(_) => {
+            eprintln!("No such file or directory");
+            process::exit(1);
+        },
+    };
 }
+ 
